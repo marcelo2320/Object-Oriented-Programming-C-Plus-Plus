e2049aba9fb07cb18094887a06a88e7dedb23341
Marcelo Peralta <nikejr122@gmail.com>
2024-01-25 15:33:24 -0800

    hello

---
+++ lab 1/hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * {this lab is to creat a code that says hello to users}
+ ***/
+
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+int main() {
+    count<<"Hello World!" << end1;
+    printf("hello world again!\n");
+    return 0;
+}
