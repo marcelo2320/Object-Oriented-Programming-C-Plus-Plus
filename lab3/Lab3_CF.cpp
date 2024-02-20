2d986fca4bb686fe55efa59524561957ebc9f556
Marcelo Peralta <nikejr122@gmail.com>
2024-02-20 15:17:13 -0800

    lab3

--- lab3/Lab3_CF.cpp
+++ lab3/Lab3_CF.cpp
@@ -6,17 +6,17 @@ int main() {
 
     int x = 0;
     int y = 5;
-    while(x<y){
+     while(x<y){
         printf("%i\n",x);
-        x++;
-    }
-    if (x%2 == 0)
-    printf("x is even\n");
-    
-    else 
-        printf("x is odd\n");
-        printf("test");
-    
+        x=x+3;
+     }
+        if (x%2==0){
+            printf("x is even\n");
+        } else{
+            printf("x is odd\n");
+        }
+          
 return 0;
+    
     
 }
